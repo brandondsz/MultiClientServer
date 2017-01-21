a02c825d5fd68ec647da1d95dcf12d7a810f0088
Brandon D'Souza <brandondsz26@gmail.com>
2017-01-21 23:25:11 +0530

    Various changes

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-var http = require('http') 
+var https = require('https')
   , bodyParser = require('body-parser')
   , express = require('express')
-  , mongodb = require('mongodb');
+  , fs = require('fs')
+  , mongodb = require('mongodb')
+  , geolib = require('geolib')
+  , os = require('os-utils');
 
 var app = express();
+var options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.crt')
+};
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -11,137 +18,238 @@ app.use(function (req, res, next) {
   next();
 });
 
+
+
+https.createServer(options, app).listen(3000, function () {
+
+  //Log CPU and Memory usage every 5 secods
+  (function () {
+    var c = 0;
+    var timeout = setInterval(function () {
+      os.cpuUsage(function (v) {
+        logCpuAndMemoryUsage(v, 1 - os.freememPercentage())
+      });
+
+      c++;
+      //Don't run out of space 
+      if (c > 1000000000) {
+        clearInterval(timeout);
+      }
+    }, 5000);
+  })();
+
+});
+
+
+
 var MongoClient = mongodb.MongoClient;
-var url = 'mongodb://localhost:27017/transport_db';
-MongoClient.connect(url, function (err, db) {
+var url = 'mongodb://127.0.0.1:27017/transport_db';
+var db;
+MongoClient.connect(url, function (err, database) {
   if (err) console.log('Unable to connect to the mongoDB server. Error:', err);
 
   app.use(bodyParser.json()); // support json encoded bodies
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  db = database;
+});
 
-  app.get('/systemHealth', function (req, res) {
-    //res.send({ id: req.params.id, name: "The Name", description: "description" });
-  });
+function logCpuAndMemoryUsage(cpu, memory) {
+  var sysHealth = {
+    "timestamp": convertDateTimeToTimeStamp(new Date()),
+    "cpu": cpu,
+    "memory": memory,
+  }
 
-  app.get('/devices', function (req, res) {
-    db.collection('devices', function (err, collection) {
-      collection.distinct('device_id', {}, function (err, items) {
-        res.send(items);
-      });
+  db.collection('systemHealth', function (err, collection) {
+    collection.insert(sysHealth, { safe: true }, function (err, result) {
+      if (err) {
+        console.log(err);
+      }
     });
   });
+}
 
-  app.get('/geoLocation/:deviceID/:fromDate/:toDate', function (req, res) {
-    //add conversion if needed
-    var fromDate = parseInt(req.params.fromDate);
-    var toDate = parseInt(req.params.toDate);
-    var deviceID = req.params.deviceID;
-    db.collection('devices').find({
-      $and:
-      [{ "device_id": deviceID },
-      {
-        $and:
-        [{ "timestamp": { $gte: fromDate } },
-        { "timestamp": { $lte: toDate } }]
-      }]
-    },
-      {
-        latitude: 1,
-        longitude: 1,
-        timestamp: 1,
-        status: 1,
-        speed: 1,
-        _id: 0
-      }).toArray(function (err, items) {
-        res.send(items);
-      })
-  })
-
-  app.get('/geoOverSpeeding/:fromDate/:toDate', function (req, res) {
-    //Assuming the required output here is a list of devices that had speed of
-    //more than 60 for more than 40 seconds(might not be 40 continuous seconds)
-
-    var fromDate = parseInt(req.params.fromDate);
-    var toDate = parseInt(req.params.toDate);
-    db.collection('devices')
-      .aggregate([
-        {
-          $match: {
-            "speed": { $gt: 60 },
-            "timestamp": { $gte: fromDate, $lte: toDate }
-          }
-        },
-        {
-          $group: {
-            _id: "$device_id",
-            count: { $sum: 1 }
-          }
-        },
-        {
-          //assuming each entry has a 10 second interval
-          $match: { count: { $gt: 4 } }
-        }
-      ])
-      .toArray(function (err, items) {
-        res.send(items);
-      });
-  })
-
-  app.get('/geoDwell/:latitude/:longitude/:fromDate/:toDate', function (req, res) {
-    //TODO: complete
-    var fromDate = parseInt(req.params.fromDate);
-    var toDate = parseInt(req.params.toDate);
-    var latitude = req.params.latitude;
-    var longitude = req.params.longitude;
-    var list = db.collection('devices').find({
+//Report 6
+app.get('/systemHealth/:fromDate/:toDate', function (req, res) {
+  var fromDate = parseInt(req.params.fromDate);
+  var toDate = parseInt(req.params.toDate);
+  db.collection('systemHealth', function (err, collection) {
+    collection.find({
       $and:
       [{ "timestamp": { $gte: fromDate } },
       { "timestamp": { $lte: toDate } }]
-    })
+    },
+      {
+        _id: 0
+      })
       .toArray(function (err, items) {
+        items = items.map(function (value) {
+          return {
+            "DateTime": convertTimeStampToDateTime(value.timestamp),
+            "CPU Usage": value.cpu * 100,
+            "Memory Usage": value.memory * 100
+          }
+        });
         res.send(items);
       });
-    // res.send(list);
-    // var result = [];
-    //   angular.forEach(list, function(value, key) {
-    //     console.log(key + ': ' + value);
-    //     this.push(key + ': ' + value);
-    //   }, result);
-    // geolib.isPointInCircle()
-  })
+  });
+});
 
-  app.post('/addData', function (req, res) {
-    var data = req.body;
-
-    //convert datetime to unix timestamp
-    var date = new Date((data.date + data.time).replace(
-      /^(\d{4})(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)$/,
-      '$4:$5:$6 $2/$3/$1'
-    ));
-    delete data.date;
-    delete data.time;
-
-    data.timestamp = convertDateTimeToTimeStamp(date)
-    data.speed = parseInt(data.speed)
-    db.collection('devices', function (err, collection) {
-      collection.insert(data, { safe: true }, function (err, result) {
-        if (err) {
-          res.send({ 'error': 'An error has occurred' });
-        } else {
-          // console.log('Success: ' + JSON.stringify(result));
-          res.send(result);
-        }
-      });
+//Report 1
+app.get('/devices', function (req, res) {
+  db.collection('devices', function (err, collection) {
+    collection.distinct('device_id', {}, function (err, items) {
+      res.send(items);
     });
   });
-
-  app.listen(3000);
-  console.log('Listening on port 3000...');
-
-
-  function convertDateTimeToTimeStamp(dateTime) {
-    return Math.floor(dateTime / 1000);
-  }
-
 });
+
+//Report 2
+app.get('/geoLocation/:deviceID/:fromDate/:toDate', function (req, res) {
+  var fromDate = parseInt(req.params.fromDate);
+  var toDate = parseInt(req.params.toDate);
+  var deviceID = req.params.deviceID;
+  db.collection('devices').find({
+    $and:
+    [{ "device_id": deviceID },
+    {
+      $and:
+      [{ "timestamp": { $gte: fromDate } },
+      { "timestamp": { $lte: toDate } }]
+    }]
+  },
+    {
+      latitude: 1,
+      longitude: 1,
+      timestamp: 1,
+      status: 1,
+      speed: 1,
+      _id: 0
+    }).toArray(function (err, items) {
+      res.send(items);
+    })
+})
+
+//Report 3
+app.get('/geoOverSpeeding/:fromDate/:toDate', function (req, res) {
+  //Assuming the required output here is a list of devices that had speed of
+  //more than 60 for more than 40 seconds(might not be 40 continuous seconds)
+
+  var fromDate = parseInt(req.params.fromDate);
+  var toDate = parseInt(req.params.toDate);
+  db.collection('devices')
+    .aggregate([
+      {
+        $match: {
+          "speed": { $gt: 60 },
+          "timestamp": { $gte: fromDate, $lte: toDate }
+        }
+      },
+      {
+        $group: {
+          _id: "$device_id",
+          count: { $sum: 1 }
+        }
+      },
+      {
+        //assuming each entry has a 10 second interval
+        $match: { count: { $gt: 4 } }
+      }
+    ])
+    .toArray(function (err, items) {
+      res.send(items.map(function (value) { return value._id }));
+    });
+})
+
+//Report 4
+app.get('/geoDwell/:latitude/:longitude/:fromDate/:toDate', function (req, res) {
+  var fromDate = parseInt(req.params.fromDate);
+  var toDate = parseInt(req.params.toDate);
+  var latitude = req.params.latitude;
+  var longitude = req.params.longitude;
+
+  getDevicesWithinTimeRange(db.collection('devices'), fromDate, toDate, function (result) {
+
+    result = result.filter(function (value) {
+      return geolib.isPointInCircle(
+        { latitude: value.latitude, longitude: value.longitude },
+        { latitude: latitude, longitude: longitude },
+        10000
+      );
+    })
+    //get only unique device_id's
+    result = result.map(function (value) { return value.device_id }).filter(function (item, i, ar) { return ar.indexOf(item) === i; });
+    res.send(result);
+  })
+})
+
+//Report 5
+app.get('/geoStationary/:fromDate/:toDate', function (req, res) {
+  var fromDate = parseInt(req.params.fromDate);
+  var toDate = parseInt(req.params.toDate);
+  db.collection('devices').aggregate([
+    {
+      $match: {
+        "speed": { $lte: 0 },
+        "timestamp": { $gte: fromDate, $lte: toDate }
+      }
+    },
+    {
+      $group: {
+        _id: "$device_id",
+        count: { $sum: 1 }
+      }
+    },
+    {
+      //assuming each entry has a 10 second interval
+      $match: { count: { $gt: 12 } }
+    }
+  ])
+    .toArray(function (err, items) {
+      res.send(items);
+    });
+})
+
+
+app.post('/addData', function (req, res) {
+  var data = req.body;
+
+  var date = new Date((data.date + data.time).replace(
+    /^(\d{4})(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)$/,
+    '$4:$5:$6 $2/$3/$1'
+  ));
+  delete data.date;
+  delete data.time;
+
+  data.timestamp = convertDateTimeToTimeStamp(date)
+  data.speed = parseInt(data.speed)
+  db.collection('devices', function (err, collection) {
+    collection.insert(data, { safe: true }, function (err, result) {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(result);
+      }
+    });
+  });
+});
+
+function getDevicesWithinTimeRange(collection, fromDate, toDate, callback) {
+  collection.find({
+    $and:
+    [{ "timestamp": { $gte: fromDate } },
+    { "timestamp": { $lte: toDate } }]
+  })
+    .toArray(function (err, items) {
+      callback(items);
+    });
+}
+
+function convertDateTimeToTimeStamp(dateTime) {
+  return Math.floor(dateTime / 1000);
+}
+function convertTimeStampToDateTime(timeStamp) {
+  return new Date(timeStamp * 1000);
+}
+
